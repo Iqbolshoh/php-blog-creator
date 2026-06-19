666c273a209cb46dec13c0219134d5a7eb08ff4c
iqbolshoh <iilhomjonbov777@gmail.com>
2026-06-19 10:27:30 +0500

    add: implement blog deletion functionality with session management

---
+++ admin/delete.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logined']) || $_SESSION['logined'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
+$blogs = json_decode(file_get_contents('../data/blogs.json'), true);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+
+    foreach ($blogs as $i => $b) {
+        if ($b['id'] == $id) {
+
+            $imagePath = '../uploads/' . $b['image'];
+            if (file_exists($imagePath)) {
+                unlink($imagePath);
+            }
+
+            unset($blogs[$i]);
+            file_put_contents('../data/blogs.json', json_encode($blogs));
+            header("Location: index.php");
+            exit;
+        }
+    }
+}
